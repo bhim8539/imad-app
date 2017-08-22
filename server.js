4f44a3a07b1522296fee989bfa646f96b1b4e70a
bhim8539 <31041451+bhim8539@users.noreply.github.com>
2017-08-22 18:01:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ var articleOne ={
     
 };
 
+function create(data){
+ var title=data.title;   
+ var date=data.date;
+ var heading=data.heading;
+ var content=data.content;
 var htmlTemplate=`
 
 <html>
@@ -61,13 +66,14 @@ var htmlTemplate=`
 
 
 `;
-
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res) {
