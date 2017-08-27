a83a0aba143ae2626826bf0e45ec43225c7cc470
bhim8539 <31041451+bhim8539@users.noreply.github.com>
2017-08-27 14:12:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
+var marginLeft='0';
+function moveRight(){
+    
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
     var interval=setInerval(moveRight,100);
